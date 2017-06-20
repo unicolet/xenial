e4021dc6e8c6da7c52a67e2f07baf40acd9e3656
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2017-06-20 11:45:53 +0200

    make ansible upgradeable

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,7 +11,7 @@ sudo -E apt-get install -y linux-headers-$(uname -r) build-essential \
      linux-image-extra-$(uname -r) \
      linux-image-extra-virtual
 
-sudo -E apt-get install -y ansible \
+sudo -E apt-get install -y ansible libssl-dev \
      wget curl jq \
      ca-certificates git \
      software-properties-common \
@@ -42,8 +42,10 @@ sudo -E add-apt-repository \
 sudo -E apt-get update
 sudo -E apt-get install -y docker-ce
 
-# @ Utils @
-sudo -E pip install ansible==2.2.1.0
+# @ Ansible @
+# uninstall system ansible
+sudo -E apt-get remove -y ansible
+sudo -E pip install --upgrade ansible==2.2.1.0
 
 # @ Filebeat @
 
@@ -51,6 +53,11 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo -E apt-key 
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo -E tee -a /etc/apt/sources.list.d/elastic-5.x.list
 sudo -E apt-get update
 sudo -E apt-get install -y filebeat
+
+# @ Vagrant insecure key @
+mkdir .ssh
+wget -O - https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub > .ssh/authorized_keys
+chmod 600 .ssh/authorized_keys
 
 # @ Clean up @
 sudo -E apt-get autoremove -y
