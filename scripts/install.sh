45159e7ce82cadcdbb05baed946c175bfbd0505f
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2017-06-20 07:56:44 +0200

    sudo

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 sudo apt update
-apt-get -y upgrade
+sudo apt-get -y upgrade
 sudo apt install -y ansible linux-headers-$(uname -r) build-essential \
      dkms apt-transport-https \
      wget curl jq \
@@ -26,24 +26,24 @@ rm /home/vagrant/VBoxGuestAdditions.iso
 #apt-get install -y oracle-java8-installer maven
 
 # @ Docker @
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
 
-apt-get update
-apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
-apt-get install -y docker-engine
-apt-get install -y git
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual
+sudo apt-get install -y docker-engine
+sudo apt-get install -y git
 
-service docker start
-docker run hello-world
+sudo service docker start
+sudo docker run hello-world
 
 # @ Utils @
-pip install awscli
-pip install ansible
+sudo pip install awscli
+sudo pip install ansible
 
 # @ Filebeat @
 
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
-apt-get update
-apt-get install filebeat
+sudo apt-get update
+sudo apt-get install filebeat
